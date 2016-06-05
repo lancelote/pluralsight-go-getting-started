71eefdce3febbd18ae84618714cf0b48b76d9b17
Pavel Karateev <karateev.pavel@ya.ru>
2016-06-05 18:54:21 +0300

    Project update

--- projects/power_plant_report.go
+++ projects/power_plant_report.go
@@ -20,19 +20,27 @@ func main() {
 
 	switch option {
 	case "1":
-		for idx, cap := range plantCapacities {
-			fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
-		}
+		generatePlantCapacityReport(plantCapacities...)
 	case "2":
-		var capacity = 0.
-		for _, plantId := range activePlants {
-			capacity += plantCapacities[plantId]
-		}
-
-		fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
-		fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
-		fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+		generatePowerGridReport(activePlants, plantCapacities, gridLoad)
 	default:
 		fmt.Printf("Unknown option, no action taken\n")
 	}
+}
+
+func generatePlantCapacityReport(plantCapacities ...float64) {
+	for idx, cap := range plantCapacities {
+		fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
+	}
+}
+
+func generatePowerGridReport(activePlants []int, plantCapacities []float64, gridLoad float64) {
+	var capacity = 0.
+	for _, plantID := range activePlants {
+		capacity += plantCapacities[plantID]
+	}
+
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
 }
